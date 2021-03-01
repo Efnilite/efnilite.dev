3eb674b8c93feb28459f0da6bc8728e2318fba37
Efnilite <re.verstappen@gmail.com>
2021-03-01 19:53:50 +0100

    Add cookies :)

--- js/general.js
+++ js/general.js
@@ -1,5 +1,7 @@
 // Dark mode
 
+let language = "en";
+let langKeys;
 let allowCookies = false;
 let darkmode = false;
 window.onload = initPage;
@@ -27,13 +29,30 @@ function logo() {
     }
 }
 
+function translate() {
+    let strings = document.getElementsByClassName("translatable");
+    for (let string of strings) {
+        let key = string.textContent;
+        string.textContent = langKeys.get(key); // todo fix
+    }
+}
+
 function initPage() {
     if (canStore()) {
+        language = localStorage.getItem("language");
+        if (language == null) {
+            language = "en";
+        }
         darkmode = localStorage.getItem("darkmode") === "true";
         if (darkmode) {
             toggleDark(false);
         }
     }
+    $.ajax({url: "./lang/" + language + ".json", dataType: 'json', async: false, success: function (gathered) {
+            langKeys = gathered;
+            console.log(gathered.length)
+            translate();
+        }});
 }
 
 /**
@@ -45,5 +64,21 @@ function canStore() {
         return 'localStorage' in window && window['localStorage'] !== null;
     } catch (e) {
         return false;
+    }
+}
+
+function toggleMenu() {
+    const list = document.getElementById("dropdown").classList;
+    list.toggle("dropdown-show");
+    if (list.contains("dropdown-show")) {
+        document.getElementById("settings").style.transform = 'rotate(90deg)';
+        let value = "toggle_on";
+        if (darkmode === false) {
+            value = "toggle_off";
+        }
+        document.getElementById("dark-toggle").innerHTML = value;
+        document.getElementById("lang-toggle").innerHTML = "<p style='font-size: 1vw'> " + langKeys.localname;
+    } else {
+        document.getElementById("settings").style.transform = 'rotate(0deg)';
     }
 }
